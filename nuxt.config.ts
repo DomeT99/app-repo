a9fac5fa305470116db333fe5daf8bdaec5d01bb
Domenico Tenace <domenicotenace99@gmail.com>
2024-03-13 23:04:13 +0100

    feat: add packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["nuxt-primevue", "@pinia/nuxt", "@vite-pwa/nuxt"],
+  css: ["primevue/resources/themes/aura-light-green/theme.css"],
+});
