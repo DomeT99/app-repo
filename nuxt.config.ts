8adac0b15bd1ede81c1076fcd1913dd28072e370
Domenico Tenace <domenicotenace99@gmail.com>
2024-03-15 17:24:28 +0100

    feat: add pwa config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,36 @@
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      title: "App Repo",
+    },
+  },
   devtools: { enabled: true },
   modules: ["nuxt-primevue", "@pinia/nuxt", "@vite-pwa/nuxt"],
   css: [
     "primevue/resources/themes/lara-dark-amber/theme.css",
     "primeicons/primeicons.css",
   ],
+  pwa: {
+    includeAssets: ["favicon.ico"],
+    manifest: {
+      name: "App Repo",
+      short_name: "App Repo",
+      description: "Little repo for your favourite apps",
+      theme_color: "#ffffff",
+      icons: [
+        {
+          src: "favicon.ico",
+          sizes: "192x192",
+          type: "image/png",
+        },
+        {
+          src: "favicon.ico",
+          sizes: "512x512",
+          type: "image/png",
+        },
+      ],
+    },
+  },
 });
