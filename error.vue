e41d36b2a2dec50a03db1cb658641d4720b37a72
Domenico Tenace <domenicotenace99@gmail.com>
2024-05-27 22:52:11 +0200

    feat: create error page

---
+++ error.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+defineProps({
+  error: Object as () => NuxtError,
+});
+
+const handleError = () => clearError({ redirect: "/" });
+</script>
+
+<template>
+  <CommonToolbar />
+  <section class="min-h-screen">
+    <div class="text-center mt-8">
+      <h2>{{ error?.statusCode }} - {{ error?.statusMessage }}</h2>
+    </div>
+    <div class="flex justify-content-center mt-6">
+      <Button @click="handleError">Return to home</Button>
+    </div>
+  </section>
+  <CommonFooter />
+</template>
